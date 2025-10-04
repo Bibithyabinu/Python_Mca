48f0b0d7b1722b516baad9d8177616c814708bea
Bibithyabinu <bibithyabinu23@gmail.com>
2025-10-03 22:46:33 -0700

    Add files via upload

---
+++ leap_year.py
@@ -0,0 +1,12 @@
+year=int(input("year"))
+print("year",year)
+if year%4==0:
+    print("leap year",year)
+else:
+    print("not leap year",year)
+
+
+    '''year 2024
+year 2024
+leap year 2024
+'''
