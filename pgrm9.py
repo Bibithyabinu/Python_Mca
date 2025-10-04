f344a4a91186a8ab0ce7c64a2a4483554b1d2bf4
Bibithyabinu <bibithyabinu23@gmail.com>
2025-10-03 22:17:39 -0700

    Delete pgrm9.py

--- pgrm9.py
+++
@@ -1,8 +0,0 @@
-basic_pay=float(input("enter your basic pay: "))
-hra=0.10*basic_pay
-ta=0.05*basic_pay
-salary=basic_pay+hra+ta
-print("basic salary",basic_pay)
-print("hra",hra)
-print("ta",ta)
-print("salary of employee",salary)
