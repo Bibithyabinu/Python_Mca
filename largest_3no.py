2290d2c86a9858cb6dcbb84d97a41a5521bd71f1
Bibithyabinu <bibithyabinu23@gmail.com>
2025-10-03 22:31:23 -0700

    Add files via upload

---
+++ largest_3no.py
@@ -0,0 +1,16 @@
+n1=int(input("first no."))
+n2=int(input("second no."))
+n3=int(input("third no."))
+if n1>n2 and n1>n3:
+    print("largest no. is",n1)
+elif n2>n3 and n2>n1 :
+    print("largest no. is",n2)
+else:
+    print("largest no. is",n3)
+
+
+    '''first no.10
+second no.200
+third no.30
+largest no. is 200
+'''
