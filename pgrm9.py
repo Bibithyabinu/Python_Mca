4a875124ee924c129668f29883319f5873820dd6
Bibithyabinu <bibithyabinu23@gmail.com>
2025-10-03 22:18:03 -0700

    Add files via upload

---
+++ pgrm9.py
@@ -0,0 +1,18 @@
+basic_pay=float(input("enter your basic pay: "))
+hra=0.10*basic_pay
+ta=0.05*basic_pay
+salary=basic_pay+hra+ta
+print("basic salary",basic_pay)
+print("hra",hra)
+print("ta",ta)
+print("salary of employee",salary)
+
+
+
+
+'''enter your basic pay: 100
+basic salary 100.0
+hra 10.0
+ta 5.0
+salary of employee 115.0
+'''
