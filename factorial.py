082d4bc800e48640b91602a86f3466ffc966c767
Bibithyabinu <bibithyabinu23@gmail.com>
2025-10-09 01:58:35 -0700

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,11 @@
+num=int(input("enter a number:"))
+fact=1
+for  i in range(2, num+1):
+    fact=fact*i
+    print("The factorial of",num,"is",fact)
+
+
+'''enter a number:4
+The factorial of 4 is 2
+The factorial of 4 is 6
+The factorial of 4 is 24
