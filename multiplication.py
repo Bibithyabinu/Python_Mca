f87a0685aa4ba0b5d175702920ebb5d41fe72707
Bibithyabinu <bibithyabinu23@gmail.com>
2025-10-09 02:19:05 -0700

    Add files via upload

---
+++ multiplication.py
@@ -0,0 +1,15 @@
+n=int(input("enter the number for multiple table:"))
+limit=int(input("enter the limit for multiple table:"))
+for i in range(1,limit+1):
+    print(n,"x",i,"=",n*i)
+
+
+'''enter the number for multiple table:3
+enter the limit for multiple table:7
+3 x 1 = 3
+3 x 2 = 6
+3 x 3 = 9
+3 x 4 = 12
+3 x 5 = 15
+3 x 6 = 18
+3 x 7 = 21'''
